ff25059145a8d28798b21918de36c2bbadab01ae
asari <32879431+deomorxsy@users.noreply.github.com>
2023-06-04 12:08:20 -0300

    add getk3s shellscript create soft links to another storage to solve node pressure eviction or taint errors

---
+++ getk3s.sh
@@ -0,0 +1,46 @@
+# =======================================
+# code adapted from example provided by @/mdrakiburrahman
+# at https://github.com/k3s-io/k3s/issues/2068#issuecomment-1374672584
+#
+# Storage prep to "/mnt/ssd/" drive (~80 GB+)
+# =======================================
+MNT_DIR="/mnt/ssd/dataStore/k3s-local/local-path-provisioner/storage"
+K3S_VERSION="v1.27.1+k3s1"
+# nodefs
+#
+KUBELET_DIR="${MNT_DIR}/kubelet"
+sudo mkdir -p "${KUBELET_DIR}"
+
+# imagefs: containerd has a root and state directory
+#
+# - https://github.com/containerd/containerd/blob/main/docs/ops.md#base-configuration
+#
+# containerd root -> /var/lib/rancher/k3s/agent/containerd
+#
+CONTAINERD_ROOT_DIR_OLD="/var/lib/rancher/k3s/agent"
+CONTAINERD_ROOT_DIR_NEW="${MNT_DIR}/containerd-root/containerd"
+sudo mkdir -p "${CONTAINERD_ROOT_DIR_OLD}"
+sudo mkdir -p "${CONTAINERD_ROOT_DIR_NEW}"
+sudo ln -s "${CONTAINERD_ROOT_DIR_NEW}" "${CONTAINERD_ROOT_DIR_OLD}"
+
+# containerd state -> /run/k3s/containerd
+#
+CONTAINERD_STATE_DIR_OLD="/run/k3s"
+CONTAINERD_STATE_DIR_NEW="${MNT_DIR}/containerd-state/containerd"
+sudo mkdir -p "${CONTAINERD_STATE_DIR_OLD}"
+sudo mkdir -p "${CONTAINERD_STATE_DIR_NEW}"
+sudo ln -s "${CONTAINERD_STATE_DIR_NEW}" "${CONTAINERD_STATE_DIR_OLD}"
+
+# pvs -> /var/lib/rancher/k3s/storage
+#
+PV_DIR_OLD="/var/lib/rancher/k3s"
+PV_DIR_NEW="${MNT_DIR}/local-path-provisioner/storage"
+sudo mkdir -p "${PV_DIR_OLD}"
+sudo mkdir -p "${PV_DIR_NEW}"
+sudo ln -s "${PV_DIR_NEW}" "${PV_DIR_OLD}"
+
+# =======
+# Install
+# =======
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="$K3S_VERSION" INSTALL_K3S_EXEC="--kubelet-arg "root-dir=$KUBELET_DIR"" sh -
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
